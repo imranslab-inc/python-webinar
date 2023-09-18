f00ddc5b997d7141608a68a3f0ae93ce0644db22
RootProgrammer <zamaan.md19@gmail.com>
2023-09-19 02:01:41 +0600

    update readme and add .py script file

---
+++ module1-python-basics/python-basics.py
@@ -0,0 +1,80 @@
+# 1. Print Statements:
+# You can use the `print()` function to display information in Python.
+
+print("Hello, World!")
+
+# 2.  Variables and Data Types:
+# Python supports various data types, including integers, floating-point numbers, strings, and more.
+
+# Variables
+age = 25
+name = "John"
+
+# Data types
+num = 42        # Integer
+pi = 3.14       # Float
+text = "Hello"  # String
+
+# 3. Comments:
+# Use the '#' symbol to add comments in your code.
+
+# This is a comment
+
+
+# 4.  Indentation:
+# Python uses indentation to define blocks of code. Typically, you use four spaces for each level of indentation.
+
+if True:
+    print("This is indented.")
+
+
+# 5.  Conditional Statements:
+# You can use `if`, `elif`, and `else` to control the flow of your program.
+
+# if condition:
+    # Code to run if the condition is True
+# elif another_condition:
+    # Code to run if the first condition is False and this one is True
+# else:
+    # Code to run if none of the conditions are True
+
+
+# 6.  Loops:
+# Python supports `for` and `while` loops for iterating over data or running code repeatedly.
+
+for i in range(5):
+    print(i)
+
+# while condition:
+    # Code to run while the condition is True
+
+
+# 7.  Lists:
+# Lists are ordered collections of items, and they can contain different data types.
+
+my_list = [1, 2, 3, "apple"]
+
+
+# 8.  Functions:
+# You can define functions to encapsulate reusable blocks of code.
+
+def greet(name):
+    print("Hello, " + name + "!")
+
+
+# 9.  Input and Output:
+# You can get input from the user using the `input()` function and display it using `print()`.
+
+user_input = input("Enter your name: ")
+print("You entered:", user_input)
+
+
+# 10.  Error Handling:
+#  Use `try` and `except` to handle exceptions and errors gracefully.
+# try:
+# Code that might raise an exception
+# except ExceptionType:
+# Code to handle the exception
+
+
+# These are some fundamental concepts in Python. As you progress, you can explore more advanced topics like classes, modules, and libraries to build more complex programs.
